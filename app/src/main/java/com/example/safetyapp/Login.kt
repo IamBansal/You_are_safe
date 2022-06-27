08f489a39e1a0f873a3a0be9518d1ca83592a23b
IamBansal <abansal11ae@gmail.com>
2022-06-27 16:15:37 +0530

    Authentication Added

--- app/src/main/java/com/example/safetyapp/Login.kt
+++ app/src/main/java/com/example/safetyapp/Login.kt
@@ -1,11 +1,16 @@
 package com.example.safetyapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -13,6 +18,7 @@ class Login : AppCompatActivity() {
     private var signIn : Button? = null
     private var email : EditText? = null
     private var password : EditText? = null
+    private var firebaseAuth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,7 @@ class Login : AppCompatActivity() {
         signIn = findViewById(R.id.btnSignIn)
         email = findViewById(R.id.etEmailLogin)
         password = findViewById(R.id.etPassLogin)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         signIn?.setOnClickListener {
             signInUser()
@@ -34,7 +41,40 @@ class Login : AppCompatActivity() {
     }
 
     private fun signInUser() {
-        startActivity(Intent(this, MainActivity::class.java))
+
+        val emailText = email?.text.toString().trim()
+        val passText = email?.text.toString().trim()
+
+        if (TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passText)) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Login failed!!")
+                .setMessage("Fill all credentials first.")
+                .setPositiveButton("Okay"){_,_-> }
+                .create()
+                .show()
+        } else {
+            val progressBar = ProgressDialog(this)
+            progressBar.setMessage("Logging in..")
+            progressBar.show()
+
+            firebaseAuth?.signInWithEmailAndPassword(emailText, passText)?.addOnCompleteListener { task ->
+                progressBar.dismiss()
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Logged in successfully.", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/safetyapp/Signup.kt
+++ app/src/main/java/com/example/safetyapp/Signup.kt
@@ -1,19 +1,27 @@
 package com.example.safetyapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class Signup : AppCompatActivity() {
 
-    private var signIn : TextView? = null
-    private var signUp : Button? = null
-    private var email : EditText? = null
-    private var password : EditText? = null
-    private var confirmPassword : EditText? = null
+    private var signIn: TextView? = null
+    private var signUp: Button? = null
+    private var email: EditText? = null
+    private var password: EditText? = null
+    private var confirmPassword: EditText? = null
+    private var firebaseAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +32,7 @@ class Signup : AppCompatActivity() {
         email = findViewById(R.id.etEmailSignup)
         password = findViewById(R.id.etPassSignup)
         confirmPassword = findViewById(R.id.etConfirmPassSignup)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         signUp?.setOnClickListener {
             signUpUser()
@@ -36,7 +45,75 @@ class Signup : AppCompatActivity() {
     }
 
     private fun signUpUser() {
-        startActivity(Intent(this, Login::class.java))
+        val emailText = email?.text.toString().trim()
+        val passwordText = password?.text.toString().trim()
+        val confirmPasswordText = confirmPassword?.text.toString().trim()
+
+        if (TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText) || TextUtils.isEmpty(confirmPasswordText)) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Signup failed!!")
+                .setMessage("Fill all credentials first.")
+                .setPositiveButton("Okay") { _, _ -> }
+                .create()
+                .show()
+        } else if (passwordText != confirmPasswordText) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Signup failed!!")
+                .setMessage("Password didn't matched.")
+                .setPositiveButton("Okay") { _, _ -> }
+                .create()
+                .show()
+        } else {
+            if (isValidPassword(passwordText)) {
+                val progressBar = ProgressDialog(this)
+                progressBar.setMessage("Signing you up..")
+                progressBar.show()
+
+                firebaseAuth?.createUserWithEmailAndPassword(emailText, passwordText)
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Signed up successfully.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            startActivity(Intent(this, Login::class.java))
+                        } else {
+                            Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Weak password...\nStrong password must contain lowercase, uppercase alphabet," +
+                            " a digit, a special character with no spaces.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        val pattern: Pattern
+        val specialCharacters = "-@%\\[\\}+'!/#$^?:;,\\(\"\\)~`.*=&\\{>\\]<_"
+
+        /*
+
+        REGEX condition explanation....
+
+        (?=.*[0-9])  This is for that it should have at least a digit.
+        (?=.*[a-z])  This is for that it should have at least a lowercase alphabet.
+        (?=.*[A-Z])  This is for that it should have at least a uppercase alphabet.
+        (?=.*[$specialCharacters])  This is for that it should have at least a special character which are defined above..
+        (?=\S+$).{8,20}  This is for that it should have at least 8 and at most 20 characters without any space..
+
+         */
+
+        val passwordRegex =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[$specialCharacters])(?=\\S+$).{8,20}$"
+        pattern = Pattern.compile(passwordRegex)
+        val matcher: Matcher = pattern.matcher(password)
+        return matcher.matches()
     }
 
     override fun onBackPressed() {
