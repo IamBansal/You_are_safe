00ba98adebd432e987986b44764a265cba025ecc
IamBansal <abansal11ae@gmail.com>
2022-06-27 16:56:45 +0530

    Contact Added

--- app/src/main/java/com/example/safetyapp/AddContact.kt
+++ app/src/main/java/com/example/safetyapp/AddContact.kt
@@ -3,9 +3,11 @@ package com.example.safetyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -55,24 +57,33 @@ class AddContact : AppCompatActivity() {
         map["relation"] = relationText
         map["number"] = numberText
 
-        //Updating user's info to realtime database
-        FirebaseDatabase.getInstance().reference.child("UsersSafety")
-            .child(firebaseAuth.currentUser!!.uid).child(numberText).updateChildren(map)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        this,
-                        "Number added successfully.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    startActivity(Intent(this, MainActivity::class.java))
-                } else {
-                    Toast.makeText(
-                        this,
-                        task.exception?.message,
-                        Toast.LENGTH_SHORT
-                    ).show()
+        if (TextUtils.isEmpty(nameText) || TextUtils.isEmpty(relationText) || TextUtils.isEmpty(
+                numberText) || numberText.length != 10) {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Adding new contact failed!!")
+                .setMessage("Fill all credentials first or check your number again.")
+                .setPositiveButton("Okay") { _, _ -> }
+                .create()
+                .show()
+        } else {
+            FirebaseDatabase.getInstance().reference.child("UsersSafety")
+                .child(firebaseAuth.currentUser!!.uid).child(numberText).updateChildren(map)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "Number added successfully.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this,
+                            task.exception?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
+        }
     }
 }
